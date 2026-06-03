675a5ad246d82664ab567072d52296302dfb7c0d
Mind <kartik.ayikkathil@mindinventory.com>
2026-06-03 15:57:16 +0530

    Added auto-distance calculation

--- constants.py
+++ constants.py
@@ -4,3 +4,10 @@ from dataclasses import dataclass
 class Tables:
     ROUTES: str = 'Routes'
     IDEMPOTENCY_KEYS: str = 'IdempotencyKeys'
+
+@dataclass(frozen=True)
+class CalculationMetrics:
+    AVG_DISTANCE_PER_SEC_FT: float = 4.5
+    DISTANCE_CAPTION_DISPLAY_DUREATION: int = 10
+    TRIGGER_DISTANCE_CAPTION_ON_DURATION: int = 45
+    DURATION_BEFORE_TURN_NOTICE: int = 8
